08dbc9b5aac4fbb7508f53ff895eb931929decf9
Vishal Tyagi <visualtaggy@gmail.com>
2022-09-20 03:48:41 +0530

    updating working model

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ import numpy as np
 import torch
 import RRDBNet_arch as arch
 
-model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
+
+model_path = 'models/UbisoftGame-Upcaling_x4.pth'
+device = torch.device('cuda')
 # device = torch.device('cpu')
 
 test_img_folder = 'LR/*'
@@ -26,12 +27,14 @@ for path in glob.glob(test_img_folder):
     # read images
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
-    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img = torch.from_numpy(np.transpose(
+        img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
     img_LR = img_LR.to(device)
 
     with torch.no_grad():
-        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = model(img_LR).data.squeeze(
+        ).float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
